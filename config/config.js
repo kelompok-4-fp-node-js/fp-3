92c15b7ac756a42fad17b9c0dd0b23d5ee5362eb
dhewan <dhewanagumm@gmail.com>
2023-05-23 19:36:13 +0700

    deploy railways 1

--- config/config.js
+++ config/config.js
@@ -2,27 +2,30 @@ require("dotenv").config();
 
 module.exports = {
   development: {
-    url :process.env.DB_URL,
+    // url :process.env.DB_URL,
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
+    port : process.env.DB_PORT,
     dialect: "postgres"
   },
   test: {
-    url :process.env.DB_URL,
+    // url :process.env.DB_URL,
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME_TEST,
     host: process.env.DB_HOST,
+    port : process.env.DB_PORT,
     dialect: "postgres",
   },
   production: {
-    url :process.env.DB_URL,
+    // url :process.env.DB_URL,
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME_PRODUCTION,
     host: process.env.DB_HOST,
+    port : process.env.DB_PORT,
     dialect: "postgres",
   },
 };
