d6e69f4969d8e8651f3bca5d59be9516987e3e87
dhewan <dhewanagumm@gmail.com>
2023-05-24 11:59:43 +0700

    add adjusment API

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
-# fp-3
-fp-3
+# final-project-3
+
+### kelompok 4
+```
+Dhewan Agum Mahendra (Leader)
+Muzammil Husaeri
+Kharis Rahmat
+```
+
+### npm command
+```
+npm i
+sequelize db:create
+sequelize db:migrate
+sequelize db:seed:all
+npm run start
+```
+### Admin account
+```
+{
+    "email":"johndoe@gmail.com",
+    "password":"josh123"
+    
+}
+```
--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,9 @@ var express = require("express");
 var express = require("express");
 var router = express.Router();
 
-
+router.get("/", (req, res) => {
+    res.json({ message: "welcome to MyGram API kelompok 4" });
+  });
 router.use('/categories', require('./category'))
 router.use("/users", require("./user"));
 router.use("/products", require("./products"));
